976f6e1544ec8f104e85eb1e7ec3a7da64c42ae4
Vesko Kenashkov <vesko@azonmedia.com>
2020-01-14 10:26:14 +0200

    wip

--- src/Parsers/CronParser.php
+++ src/Parsers/CronParser.php
@@ -6,4 +6,12 @@ namespace Azonmedia\Cron\Parsers;
 class CronParser
 {
 
+    /**
+     * CronParser constructor.
+     * @param string $cron_file The path to the cron file
+     */
+    public function __construct(string $cron_file)
+    {
+
+    }
 }
--- src/Parsers/PhpParser.php
+++ src/Parsers/PhpParser.php
@@ -3,7 +3,33 @@ declare(strict_types=1);
 
 namespace Azonmedia\Cron\Backends;
 
-class PhpParser
+use Azonmedia\Cron\Interfaces\ParserInterface;
+
+class PhpParser implements ParserInterface
 {
 
+    /**
+     * PhpParser constructor.
+     * The config contains php callables
+     * @param array $config
+     */
+    public function __construct(array $config)
+    {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function get_all_jobs(): iterable
+    {
+        // TODO: Implement get_all_jobs() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function get_current_jobs(): iterable
+    {
+        // TODO: Implement get_current_jobs() method.
+    }
 }
